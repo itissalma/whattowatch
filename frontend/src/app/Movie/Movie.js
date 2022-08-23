dd576f02903a74f9fedbcd932a988ec7470f1c35
nyuriumuri <nyuriumuri@gmail.com>
2022-08-23 11:37:10 +0200

    getNextMovie and getPrevMovie functions

--- frontend/src/app/Movie/Movie.js
+++ frontend/src/app/Movie/Movie.js
@@ -1,6 +1,23 @@
 import React from "react"
+import { useState } from "react"
 
-const Movie = () =>{
+const Movie = (props) =>{
+    const {movieList} = props
+    const empty = movieList.length <= 0
+    const [curMovieIndex, setCurMovieIndex ]  = movieList.length ? 0 : -1
+    const [curMovie, setCurMovie] = movieList.length ? movieList[0] : {}
+    const getNextMovie = () =>{
+        if(empty || curMovieIndex >= movieList.length-1) return;
+        let curIndex = curMovieIndex +1 
+        setCurMovieIndex(curIndex)
+        setCurMovie(movieList[curIndex])
+    }
+    const getPrevMovie = () =>{
+        if(empty || curMovieIndex <=0) return;
+        let curIndex = curMovieIndex-1;
+        setCurMovieIndex(curIndex)
+        setCurMovie(movieList[curIndex])
+    }
     return (
     <div>
         Movie!!!
